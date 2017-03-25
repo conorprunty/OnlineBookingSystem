1c45ad85a65faaa83245aeb0fceca7fa444f61ce
conorprunty <conorprunty@hotmail.com>
2017-03-25 11:40:18 +0000

    Revert "added reference"
    This reverts commit 141e855be71a92d5ae8f3cbe5954f07e3ebd683c.

--- js/date.js
+++ js/date.js
@@ -1,5 +1,3 @@
-//https://www.experts-exchange.com/questions/28155045/getting-the-date-of-monday-and-sunday-of-the-current-week.html
-
 var today = new Date();
 var dayOfWeekStartingSundayZeroIndexBased = today.getDay(); 
 var sundayOfWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay()+7);
