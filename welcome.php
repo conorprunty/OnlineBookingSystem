53924927070794fd14c98ab8630cd9e4d4196fc5
conorprunty <conorprunty@hotmail.com>
2017-03-25 11:49:03 +0000

    Revert "Revert "reversed last change""
    This reverts commit 9c5c78994f7afdf05f74ba710e3adc19e724d19b.

--- welcome.php
+++ welcome.php
@@ -23,15 +23,15 @@
     <link href="style/style.css" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/banner.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    var icon = <?php echo $bannerresult["icon"];?>;
+    </script>
   </head>
   <body onload="setBanner()">
       <header>
             <div id="icon"></div>
         </header>
-      <script src="js/banner.js" type="text/javascript"></script>
-      <script type="text/javascript">
-    var icon = <?php echo $bannerresult["icon"];?>;
-    </script>
       
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
