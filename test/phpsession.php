b79699ab66d12c321042227ace5a4d1a4c550c58
conorprunty <conorprunty@hotmail.com>
2017-05-06 23:16:08 +0100

    removed all unnecessary files

--- test/phpsession.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $username = "root";
-	$password = "root";
-	$host = "localhost";
-	$dbname = "obsadmintest";
-	// Create connection
-	$mysqli = new mysqli($host, $username, $password, $dbname);
-?>
